134b53cda960a71c8b6b1600aee2cacb1c9c0d45
Sami Osborn <samiosborn@live.co.uk>
2026-04-12 21:09:12 +0100

    Added Pi

--- firmware/include/balancebot/core/physics_constants.hpp
+++ firmware/include/balancebot/core/physics_constants.hpp
@@ -5,11 +5,14 @@ namespace balancebot {
 
 // Physics constants
 struct PhysicsConstants {
+    // Pi
+    static constexpr float pi = 3.14159265358979323846f;
+
     // Standard gravitational acceleration (m/s^2)
     static constexpr float gravity_m_s2 = 9.80665f;
 
     // Degrees to radians conversion factor
-    static constexpr float deg_to_rad = 3.14159265358979323846f / 180.0f;
+    static constexpr float deg_to_rad = pi / 180.0f;
 };
 
 }  // namespace balancebot
